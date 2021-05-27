57a0e1d6dd02714f439592f4321b63602354df8b
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-05-27 21:52:25 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,19 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :use
+  has_one_attached :image
 
   validates :content, presence: true
-end
 
+
+
+validates :content, presence: true, unless: :was_attached?
+
+
+
+private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+end
